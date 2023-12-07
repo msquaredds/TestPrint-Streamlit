e04ba51c9d30c5d34eab7aa20eabca33b0dd5506
msquaredds <alex.melesko@msquaredds.com>
2023-12-07 13:24:22 -0500

    Building out the test

--- pages/1_TestPage.py
+++ pages/1_TestPage.py
@@ -2,14 +2,14 @@ import streamlit as st
 
 title_cols = st.columns(3)
 with title_cols[1]:
-    title_writing = "Test Title"
+    title_writing = "Test Title 2"
     title_format = f'<p style="text-align: center; font-family: ' \
                    f'Arial; color: #808080; font-size: 40px; ' \
                    f'font-weight: bold;">{title_writing}</p>'
     st.markdown(title_format, unsafe_allow_html=True)
 subtitle_cols = st.columns(3)
 with subtitle_cols[1]:
-    sub_title_writing = "Test Subtitle"
+    sub_title_writing = "Test Subtitle 2"
     subtitle_format = (
         f'<p style="text-align: center; font-family: Arial; '
         f'color: #808080; font-size: 32px; font-weight: bold;">'
