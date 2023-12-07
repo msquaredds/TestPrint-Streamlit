bf355136419efe3e1e02920b71aa5de523f12aad
msquaredds <alex.melesko@msquaredds.com>
2023-12-07 13:36:13 -0500

    Building out the test

--- pages/1_TestPage.py
+++ pages/1_TestPage.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
-st.write("Test Title 2")
+title_writing = "Test Title 2"
+title_format = f'<p style="text-align: center; font-family: ' \
+               f'Arial; color: #808080; font-size: 40px; ' \
+               f'font-weight: bold;">{title_writing}</p>'
+st.markdown(title_format, unsafe_allow_html=True)
