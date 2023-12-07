b3f63863db9f534a027a157e731ed8a3853d870b
msquaredds <alex.melesko@msquaredds.com>
2023-12-07 13:27:01 -0500

    Building out the test

--- pages/1_TestPage.py
+++ pages/1_TestPage.py
@@ -7,11 +7,3 @@ with title_cols[1]:
                    f'Arial; color: #808080; font-size: 40px; ' \
                    f'font-weight: bold;">{title_writing}</p>'
     st.markdown(title_format, unsafe_allow_html=True)
-subtitle_cols = st.columns(3)
-with subtitle_cols[1]:
-    sub_title_writing = "Test Subtitle 2"
-    subtitle_format = (
-        f'<p style="text-align: center; font-family: Arial; '
-        f'color: #808080; font-size: 32px; font-weight: bold;">'
-        f'{sub_title_writing}</p>')
-    st.markdown(subtitle_format, unsafe_allow_html=True)
